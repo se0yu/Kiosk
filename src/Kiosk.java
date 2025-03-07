02ee86634bd76208fe1b69170c06bb1399318928
se0yu <pr924133@gmail.com>
2025-03-07 17:41:22 +0900

    키오스크 level3

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
 
@@ -7,13 +8,18 @@ public class Kiosk {
 
     private List<MenuItem> menuItems = new ArrayList<>();
 
+
+    //메뉴 리스트 추가
     private void addMenuList(){
-        menuItems.add(new MenuItem("햄부가티온앤온",6.5,"햄버거"));
         menuItems.add(new MenuItem("ShackBurger",6.9,"토마토, 양상추, 쉑소스가 토핑된 치즈버거"));
-        menuItems.add(new MenuItem("햄버거",6.5,"햄버거"));
-        menuItems.add(new MenuItem("햄버거",6.5,"햄버거"));
+        menuItems.add(new MenuItem("SmokeShack",8.9,"베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Cheeseburger",6.5,"포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"));
+        menuItems.add(new MenuItem("Hamburger",5.4,"비프패티를 기반으로 야채가 들어간 기본버거"));
+
     }
 
+
+    //생성자
     public Kiosk() {
         addMenuList();
     }
@@ -22,25 +28,35 @@ public class Kiosk {
     //메뉴판 화면 출력
     public void printMenu(){
         while (true){
+            //메뉴 리스트 출력
             System.out.println("============ MENU =============");
             for (int i = 0; i < menuItems.size(); i++) {
                 MenuItem menuItem = menuItems.get(i);
-                System.out.println(i + 1 + "." + menuItem.productName + " | " + menuItem.productPrice + " | " + menuItem.productInfo);
+                System.out.println(i + 1 + "." + menuItem.productName + " | W " + menuItem.productPrice + " | " + menuItem.productInfo);
             }
             System.out.println("0. 키오스크 종료 ");
-            int selectMenu = scanner.nextInt();
-            if (selectMenu == 0) {
-                return;
-            }else{
-                    MenuItem menuItem = menuItems.get(selectMenu-1);
-                    System.out.println("선택한 메뉴 : "+menuItem.productName+" " +menuItem.productPrice+ " "+ menuItem.productInfo);
+            //메뉴 번호 입력 (0 입력시 프로그램 종료)
+            try {
+                int selectMenu = scanner.nextInt();
+                if (selectMenu == 0) {
+                    return;
+                } else {
+                    MenuItem menuItem = menuItems.get(selectMenu - 1);
+                    System.out.println("선택한 메뉴 : " + menuItem.productName + " " + menuItem.productPrice + " " + menuItem.productInfo + "\n");
                 }
+            } catch (IndexOutOfBoundsException indexOutOfBoundsException) {
+                System.out.println("잘못된 입력입니다. 다시 입력해주세요.\n");
+            } catch (InputMismatchException inputMismatchException) {
+                System.out.println("잘못된 입력입니다. 숫자를 입력해주세요.\n");
+                scanner.nextLine();
+            }
+            }
 
             }
 
         }
 
-    }
+
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,7 @@
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
         Kiosk kiosk = new Kiosk();
-
         kiosk.printMenu();
         }
     }
